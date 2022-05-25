feea22c2018c3e744780df72a5b5a78eed66c50e
DiegoFelipe7 <90659322+DiegoFelipe7@users.noreply.github.com>
2022-05-25 18:55:09 -0500

    Initial commit

---
+++ src/main/java/com/sofka/classes/Calculator.java
@@ -0,0 +1,8 @@
+package com.sofka.classes;
+
+public class Calculator {
+    public double sum(double num1 , double num2 ){return num1+num2;}
+    public double subtract(double num1 , double num2 ){return num1-num2;}
+    public double multiplication(double num1 , double num2 ){return num1*num2;}
+    public double split(double num1 , double num2 ){return num1/num2;}
+}
