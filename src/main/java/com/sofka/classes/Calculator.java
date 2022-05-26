47ba72b95f4da681b9391a339ce662e4809aadcd
DiegoFelipe7 <90659322+DiegoFelipe7@users.noreply.github.com>
2022-05-25 21:21:52 -0500

    Creacion del main

--- src/main/java/com/sofka/classes/Calculator.java
+++
@@ -1,8 +0,0 @@
-package com.sofka.classes;
-
-public class Calculator {
-    public double sum(double num1 , double num2 ){return num1+num2;}
-    public double subtract(double num1 , double num2 ){return num1-num2;}
-    public double multiplication(double num1 , double num2 ){return num1*num2;}
-    public double split(double num1 , double num2 ){return num1/num2;}
-}
--- src/main/java/com/sofka/main/Main.java
+++ src/main/java/com/sofka/main/Main.java
@@ -8,24 +8,33 @@ public class Main {
         Logger logger = Logger.getLogger(Main.class.getName());
         Scanner scanner = new Scanner(System.in);
         String operation = "Welcome a to calculator \n 1.Sum \n 2.Subtract \n 3.Multiplication \n 4.Split \n 5.Exit";
-        do {
-                logger.info(operation);
-                int action=scanner.nextInt();
+        int action;
+        do{
+            logger.info(operation);
+            action=scanner.nextInt();
+            if(action != 5){
                 logger.info("Enter the first value");
                 int num1 = scanner.nextInt();
                 logger.info("Enter the second value");
                 int num2 = scanner.nextInt();
                 actions(action,num1,num2,logger);
-        }while (true);
+            }
+        }while (action != 5);
     }
     public static void actions(int action , int num1 , int num2 , Logger logger){
         Calculator calculator = new Calculator();
         switch (action) {
-            case 1 -> calculator.sum(num1, num2);
-            case 2 -> calculator.subtract(num1, num2);
-            case 3 -> calculator.multiplication(num1, num2);
-            case 4 -> calculator.split(num1, num2);
-            default -> logger.info("invalid option");
+            case 1 -> result(calculator.sum(num1, num2));
+            case 2 -> result(calculator.subtract(num1, num2));
+            case 3 -> result(calculator.multiplication(num1, num2));
+            case 4 -> result(calculator.split(num1, num2));
+            case 5-> logger.info("Exit.");
+            default -> logger.info("error");
         }
+    }
+
+    public static void result(double result){
+        Logger logger = Logger.getLogger(Main.class.getName());
+        logger.info(()->"the result is "+result);
     }
 }
